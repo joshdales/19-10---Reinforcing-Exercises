019a5e5ce36732829e272cb1fe7396bc4cea58ac
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-19 14:21:07 -0400

    define method and add return

--- exercise.rb
+++ exercise.rb
@@ -5,18 +5,23 @@ seats = [
   ["Smokey","Toast","Pacha","Mau"]
 ]
 
-seats.each_with_index do |row, r_index|
-  row.each_with_index do |seat, s_index|
-    if seat == nil
-      puts "Row #{r_index + 1 } seat #{s_index + 1} is free. Do you want to sit there? (y/n)"
-      wanna_seat = gets.chomp
-      if wanna_seat == 'y'
-        puts "What is your name?"
-        your_name = gets.chomp
-        row[s_index] = your_name
-        p seats
-        return
+
+def seating_plan(classroom)
+  classroom.each_with_index do |row, r_index|
+    row.each_with_index do |seat, s_index|
+      if seat == nil
+        puts "Row #{r_index + 1 } seat #{s_index + 1} is free. Do you want to sit there? (y/n)"
+        wanna_seat = gets.chomp
+        if wanna_seat == 'y'
+          puts "What is your name?"
+          your_name = gets.chomp
+          row[s_index] = your_name
+          p classroom
+          return
+        end
       end
     end
   end
 end
+
+seating_plan(seats)
