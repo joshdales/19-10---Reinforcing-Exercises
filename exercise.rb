95efac7f95fe230f1b91d4e6373444cb2ba1dbb9
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-19 13:33:20 -0400

    Display who is sitting in each seat

--- exercise.rb
+++ exercise.rb
@@ -1,6 +1,12 @@
-[
+seats = [
   [nil, "Pumpkin", nil, nil],
   ["Socks", nil, "Mimi", nil],
   ["Gismo", "Shadow", nil, nil],
   ["Smokey","Toast","Pacha","Mau"]
 ]
+
+seats.each_with_index do |row, index|
+  row.each do |seat|
+    puts "Row #{index + 1} seat #{seat}"
+  end
+end
